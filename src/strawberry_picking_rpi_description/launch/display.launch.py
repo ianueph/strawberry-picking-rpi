d45fdacf6b6d5ca7b5ed3e2c50fb6a4cd125d4f9
ianueph <daniel.angeloianmiguel@ue.edu.ph>
2025-03-14 15:01:12 +0000

    Create display.launch.py

--- src/strawberry_picking_rpi_description/launch/display.launch.py
+++ src/strawberry_picking_rpi_description/launch/display.launch.py
@@ -1,19 +1,17 @@
 from launch import LaunchDescription
-from launch.action import PathJoinSubstitution
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
-import xacro
+
 
 def generate_launch_description():
     ld = LaunchDescription()
-    
-    default_model_path = PathJoinSubstitution([
-        FindPackageShare('strawberry_picking_rpi_description'),
-        'urdf', 
-        'strawberry_picking_rpi.urdf.xacro'
-        ])    
-    
-    xacro_to_urdf = xacro.process(default_model_path)
-    
-    
-    
+
+    ld.add_action(IncludeLaunchDescription(
+        PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
+        launch_arguments={
+            'urdf_package': 'strawberry_picking_rpi_description',
+            'urdf_package_path': PathJoinSubstitution(['urdf', 'strawberry_picking_rpi.urdf.xacro'])}.items()
+    ))
+
     return ld
