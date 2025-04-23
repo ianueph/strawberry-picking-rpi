79df05b3d74b48f213bc990483262403e2418c20
ianueph <daniel.angeloianmiguel@ue.edu.ph>
2025-04-23 17:17:18 +0000

    Add depth pipeline

--- src/sprpi_arm_bringup/setup.py
+++ src/sprpi_arm_bringup/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'sprpi_arm_bringup'
 
@@ -7,8 +9,7 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.py'))),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
