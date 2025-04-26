35b2396c7339eb7a4eb7ef56748b9463b96eea6b
ianueph <daniel.angeloianmiguel@ue.edu.ph>
2025-04-26 01:38:12 +0000

    Create node to create tasks for the robot based on detections from the yolo nodes

---
+++ src/sprpi_arm_mtc/src/main_task_node.cpp
@@ -0,0 +1,324 @@
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/robot_state/cartesian_interpolator.h>
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit/task_constructor/task.h>
+#include <moveit/task_constructor/solvers.h>
+#include <moveit/task_constructor/stages.h>
+#include <yolo_msgs/msg/detection_array.hpp>
+#if __has_include(<tf2_geometry_msgs/tf2_geometry_msgs.hpp>)
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#else
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#endif
+#if __has_include(<tf2_eigen/tf2_eigen.hpp>)
+#include <tf2_eigen/tf2_eigen.hpp>
+#else
+#include <tf2_eigen/tf2_eigen.h>
+#endif
+
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("main_task_node");
+namespace mtc = moveit::task_constructor;
+
+class SprpiMainTaskNode
+{
+public: 
+
+	SprpiMainTaskNode(const rclcpp::NodeOptions& options);
+    rclcpp::node_interfaces::NodeBaseInterface::SharedPtr getNodeBaseInterface();
+	void initPlanners();
+	void depthDetectionsCallback(const yolo_msgs::msg::DetectionArray::SharedPtr msg);
+	void mirrorDetectionsCallback(const yolo_msgs::msg::DetectionArray::SharedPtr msg);
+    mtc::Task createPickTask(const std::string object_id);
+    mtc::Task createPlaceTask();
+
+private:
+
+    struct IKParameters {
+        int64_t angle_delta;
+        double min_solution_distance;
+        int64_t max_ik_solutions;
+        double jump_threshold;
+    };
+
+    IKParameters ik_params_;
+
+    rclcpp::Node::SharedPtr node_;
+	std::string depth_detections_topic_name_;
+    std::string mirror_detections_topic_name_;
+    rclcpp::Subscription<yolo_msgs::msg::DetectionArray>::SharedPtr depth_detections_subscription_;
+    rclcpp::Subscription<yolo_msgs::msg::DetectionArray>::SharedPtr mirror_detections_subscription_;
+    yolo_msgs::msg::DetectionArray depth_detections_;
+    yolo_msgs::msg::DetectionArray mirror_detections_;
+
+    std::string arm_group_name_ = "arm";
+    std::string hand_group_name_ = "Gripper";
+    std::string hand_frame_ = "gripper_base";
+
+    std::shared_ptr<mtc::solvers::PipelinePlanner> sampling_planner_;
+    std::shared_ptr<mtc::solvers::JointInterpolationPlanner> interpolation_planner_;
+    std::shared_ptr<mtc::solvers::CartesianPath> cartesian_planner_;
+
+	std::set<std::string> previous_object_ids_;
+};
+
+SprpiMainTaskNode::SprpiMainTaskNode(const rclcpp::NodeOptions& options)
+  : node_{ std::make_shared<rclcpp::Node>("main_task_node", options) }
+{
+    // Task planning parameters
+    ik_params_.angle_delta = node_->get_parameter("angle_delta").as_int();
+    ik_params_.min_solution_distance = node_->get_parameter("min_solution_distance").as_double();
+    ik_params_.max_ik_solutions = node_->get_parameter("max_ik_solutions").as_int();
+    ik_params_.jump_threshold = node_->get_parameter("jump_threshold").as_double();
+
+    // Robot description parameters
+    arm_group_name_ = node_->get_parameter("arm_group_name_").as_string();
+    hand_group_name_ = node_->get_parameter("hand_group_name_").as_string();
+    hand_frame_ = node_->get_parameter("hand_frame_").as_string();
+
+	depth_detections_topic_name_ = node_->get_parameter("depth_detections_topic_name_").as_string();
+    mirror_detections_topic_name_ = node_->get_parameter("mirror_detections_topic_name_").as_string();
+
+    depth_detections_subscription_= node_->create_subscription<yolo_msgs::msg::DetectionArray>(
+        "/yolo/depth/detections", 
+        10, 
+        std::bind(&SprpiMainTaskNode::depthDetectionsCallback, this, std::placeholders::_1)
+    );
+
+    mirror_detections_subscription_= node_->create_subscription<yolo_msgs::msg::DetectionArray>(
+        "/yolo/mirror/detections", 
+        10, 
+        std::bind(&SprpiMainTaskNode::mirrorDetectionsCallback, this, std::placeholders::_1)
+    );
+}
+
+rclcpp::node_interfaces::NodeBaseInterface::SharedPtr SprpiMainTaskNode::getNodeBaseInterface()
+{
+  return node_->get_node_base_interface();
+}
+
+void SprpiMainTaskNode::initPlanners()
+{
+    sampling_planner_ = std::make_shared<mtc::solvers::PipelinePlanner>(node_);
+    interpolation_planner_ = std::make_shared<mtc::solvers::JointInterpolationPlanner>();
+
+    cartesian_planner_ = std::make_shared<mtc::solvers::CartesianPath>();
+    cartesian_planner_->setMaxVelocityScalingFactor(1.0);
+    cartesian_planner_->setMaxAccelerationScalingFactor(1.0);
+    cartesian_planner_->setStepSize(.01);
+    cartesian_planner_->setJumpThreshold(ik_params_.jump_threshold);
+}
+
+void SprpiMainTaskNode::depthDetectionsCallback(const yolo_msgs::msg::DetectionArray::SharedPtr msg)
+{   
+    std::vector<moveit_msgs::msg::CollisionObject> collision_objects;
+	std::set<std::string> current_object_ids;
+	collision_objects.reserve(msg->detections.size());
+
+    for (const auto &detection : msg->detections) {
+		auto id = detection.id;
+		auto class_name = detection.class_name;
+		auto frame_id =  detection.bbox3d.frame_id;
+		auto dimensions = detection.bbox3d.size;
+		auto pose = detection.bbox3d.center;
+
+		moveit_msgs::msg::CollisionObject object;
+		object.id = id + "_" + class_name;
+		object.header.frame_id = frame_id;
+		object.primitives.resize(1);
+		object.primitives[0].type = shape_msgs::msg::SolidPrimitive::BOX;
+		object.primitives[0].dimensions = { dimensions.x, dimensions.y, dimensions.z };
+		object.pose = pose;
+
+		current_object_ids.insert(id);
+		collision_objects.push_back(object);
+    }
+
+	moveit::planning_interface::PlanningSceneInterface psi;
+	std::vector<std::string> missing_ids;
+	std::set_symmetric_difference(	current_object_ids.begin(), 	current_object_ids.end(),
+									previous_object_ids_.begin(), 	previous_object_ids_.end(),
+									std::back_inserter(missing_ids));
+	psi.removeCollisionObjects(missing_ids);
+
+    if (!psi.applyCollisionObjects(collision_objects)) {
+		RCLCPP_ERROR(LOGGER, "Failed to apply collision objects");
+	}
+
+	previous_object_ids_ = current_object_ids;
+}
+
+void SprpiMainTaskNode::mirrorDetectionsCallback(const yolo_msgs::msg::DetectionArray::SharedPtr msg)
+{   
+
+}
+
+mtc::Task SprpiMainTaskNode::createPickTask(const std::string object_id)
+{
+    mtc::Task task;
+    task.stages()->setName("pick_task");
+    task.loadRobotModel(node_);
+  
+    // Set task properties
+    task.setProperty("group", arm_group_name_);
+    task.setProperty("eef", hand_group_name_);
+    task.setProperty("ik_frame", hand_frame_);
+// Disable warnings for this line, as it's a variable that's set but not used in this example
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+  	mtc::Stage* current_state_ptr = nullptr;  // Forward current_state on to grasp pose generator
+#pragma GCC diagnostic pop
+
+    auto stage_state_current = std::make_unique<mtc::stages::CurrentState>("current");
+    current_state_ptr = stage_state_current.get();
+    task.add(std::move(stage_state_current));
+
+    auto stage_default_position = std::make_unique<mtc::stages::MoveTo>("move to default", interpolation_planner_);
+    stage_default_position->setGroup(arm_group_name_);
+    stage_default_position->setGoal("Default");
+    task.add(std::move(stage_default_position));
+
+    auto stage_open_hand = std::make_unique<mtc::stages::MoveTo>("open hand", interpolation_planner_);
+    stage_open_hand->setGroup(hand_group_name_);
+    stage_open_hand->setGoal("open");
+    task.add(std::move(stage_open_hand));
+
+    mtc::Stage* attach_object_stage =
+    nullptr;  // Forward attach_object_stage to place pose generator
+
+    {
+        auto grasp = std::make_unique<mtc::SerialContainer>("pick object");
+        task.properties().exposeTo(grasp->properties(), { "eef", "group", "ik_frame" });
+        grasp->properties().configureInitFrom(mtc::Stage::PARENT,
+                                              { "eef", "group", "ik_frame" });
+        {
+          auto stage =
+              std::make_unique<mtc::stages::MoveRelative>("approach object", cartesian_planner_);
+          stage->properties().set("marker_ns", "approach_object");
+          stage->properties().set("link", hand_frame_);
+          stage->properties().configureInitFrom(mtc::Stage::PARENT, { "group" });
+          stage->setMinMaxDistance(0.085, 0.15);
+        
+          // Set hand forward direction
+          geometry_msgs::msg::Vector3Stamped vec;
+          vec.header.frame_id = hand_frame_;
+          vec.vector.z = 1.0;
+          stage->setDirection(vec);
+          grasp->insert(std::move(stage));
+        }
+    
+        {
+          // Sample grasp pose
+          auto stage = std::make_unique<mtc::stages::GenerateGraspPose>("generate grasp pose");
+          stage->properties().configureInitFrom(mtc::Stage::PARENT);
+          stage->properties().set("marker_ns", "grasp_pose");
+          stage->setPreGraspPose("open");
+          stage->setObject(object_id);
+          stage->setAngleDelta(M_PI / ik_params_.angle_delta);
+          stage->setMonitoredStage(current_state_ptr);  // Hook into current state
+    
+          Eigen::Isometry3d grasp_frame_transform;
+          Eigen::Quaterniond q = Eigen::AngleAxisd(M_PI / 2, Eigen::Vector3d::UnitX()) *
+                                Eigen::AngleAxisd(M_PI / 2, Eigen::Vector3d::UnitY()) *
+                                Eigen::AngleAxisd(M_PI / 2, Eigen::Vector3d::UnitZ());
+          grasp_frame_transform.linear() = q.matrix();
+          grasp_frame_transform.translation().z() = 0.1;
+    
+            // Compute IK
+          auto wrapper =
+          std::make_unique<mtc::stages::ComputeIK>("grasp pose IK", std::move(stage));
+          wrapper->setMaxIKSolutions(ik_params_.max_ik_solutions);
+          wrapper->setMinSolutionDistance(ik_params_.min_solution_distance);
+          wrapper->setIKFrame(grasp_frame_transform, hand_frame_);
+          wrapper->properties().configureInitFrom(mtc::Stage::PARENT, { "eef", "group" });
+          wrapper->properties().configureInitFrom(mtc::Stage::INTERFACE, { "target_pose" });
+          grasp->insert(std::move(wrapper));
+        }
+    
+        {
+          auto stage =
+              std::make_unique<mtc::stages::ModifyPlanningScene>("allow collision (hand,object)");
+          stage->allowCollisions(object_id,
+                                task.getRobotModel()
+                                    ->getJointModelGroup(hand_group_name_)
+                                    ->getLinkModelNamesWithCollisionGeometry(),
+                                true);
+          grasp->insert(std::move(stage));
+        }
+    
+        {
+          auto stage = std::make_unique<mtc::stages::MoveTo>("close hand", interpolation_planner_);
+          stage->setGroup(hand_group_name_);
+          stage->setGoal("close");
+          grasp->insert(std::move(stage));
+        }
+    
+        {
+          auto stage = std::make_unique<mtc::stages::ModifyPlanningScene>("attach object");
+          stage->attachObject(object_id, hand_frame_);
+          attach_object_stage = stage.get();
+          grasp->insert(std::move(stage));
+        }
+    
+        {
+          auto stage =
+              std::make_unique<mtc::stages::MoveRelative>("retreat from object", cartesian_planner_);
+          stage->properties().set("marker_ns", "retreat_from_object");
+          stage->properties().set("link", hand_frame_);
+          stage->properties().configureInitFrom(mtc::Stage::PARENT, { "group" });
+          stage->setMinMaxDistance(0.085, 0.15);
+        
+          // Set hand forward direction
+          geometry_msgs::msg::Vector3Stamped vec;
+          vec.header.frame_id = hand_frame_;
+          vec.vector.z = -1.0;
+          stage->setDirection(vec);
+          grasp->insert(std::move(stage));
+        }
+    
+        {
+          auto stage =
+              std::make_unique<mtc::stages::MoveTo>("bring object infront of mirror", sampling_planner_);
+          stage->properties().configureInitFrom(mtc::Stage::PARENT, { "group" });
+          stage->setGroup(arm_group_name_);
+          stage->setGoal("infront_of_mirror");
+          stage->properties().set("marker_ns", "infront_of_mirror");
+    
+          grasp->insert(std::move(stage));
+        }
+    
+        task.add(std::move(grasp));
+      }
+    
+    return task;
+}
+
+mtc::Task SprpiMainTaskNode::createPlaceTask()
+{
+	mtc::Task task;
+	return task;
+}
+
+int main(int argc, char** argv)
+{
+  rclcpp::init(argc, argv);
+
+  rclcpp::NodeOptions options;
+  options.automatically_declare_parameters_from_overrides(true);
+
+  auto main_task_node = std::make_shared<SprpiMainTaskNode>(options);
+  rclcpp::executors::MultiThreadedExecutor executor;
+
+  auto spin_thread = std::make_unique<std::thread>([&executor, &main_task_node]() {
+    executor.add_node(main_task_node->getNodeBaseInterface());
+    executor.spin();
+    executor.remove_node(main_task_node->getNodeBaseInterface());
+  });
+
+  main_task_node->initPlanners();
+
+  spin_thread->join();
+  rclcpp::shutdown();
+  return 0;
+}
+
