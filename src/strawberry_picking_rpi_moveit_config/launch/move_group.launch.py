1f19ea3e17d3111236368e60b6edaf308aa64dc9
ianueph <daniel.angeloianmiguel@ue.edu.ph>
2025-05-03 20:34:48 +0000

    Add capability to movegroup launch

--- src/strawberry_picking_rpi_moveit_config/launch/move_group.launch.py
+++ src/strawberry_picking_rpi_moveit_config/launch/move_group.launch.py
@@ -4,4 +4,8 @@ from moveit_configs_utils.launches import generate_move_group_launch
 
 def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("strawberry_picking_rpi", package_name="strawberry_picking_rpi_moveit_config").to_moveit_configs()
+    moveit_config.move_group_capabilities = {
+        "capabilities": "move_group/ExecuteTaskSolutionCapability",
+        "disable_capabilities": "",
+    }
     return generate_move_group_launch(moveit_config)
